8571ce8dc21801d70d803ba297cd6288472be4a0
Tim Huang <tim.huang61@gmail.com>
2019-01-31 02:09:37 +0800

    Update slack_notify.groovy

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -14,8 +14,8 @@ def call(String channel, String buildStatus) {
     }
 
     def job = "${env.JOB_NAME} - #${env.BUILD_NUMBER} "
-    def normal_msg = "${status} after ${currentBuild.durationString}. (<${env.BUILD_URL}|#Open>)"
-    def start_msg = "${user} triggered this job. (<${env.BUILD_URL}|#Open>)"
+    def normal_msg = "${status} after ${currentBuild.durationString}. (<${env.BUILD_URL}|Open>)"
+    def start_msg = "${user} triggered this job. (<${env.BUILD_URL}|Open>)"
     def message = job + (status == 'STARTED' ? start_msg : normal_msg)
    
     slackSend (channel: channel, color: color, message: message)
