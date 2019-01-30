e5be55a494fce60cbc68c24e2ae64ef51493d612
Tim <tim.huang61@gmail.com>
2019-01-31 02:02:02 +0800

    test

--- vars/slack_notify.groovy
+++ vars/slack_notify.groovy
@@ -1,6 +1,6 @@
 def call(String channel, String buildStatus) {
     def status = buildStatus ?: 'SUCCESS'
-    def color = ''
+    def user = 'unknown'
     def colorLookup = [
         'STARTED':'',
         'SUCCESS':'good',
@@ -8,15 +8,15 @@ def call(String channel, String buildStatus) {
         'NOT_BUILT':'',
         'UNSTABLE':'danger']
 
-    if(colorLookup.containsKey(status)){
-        color = colorLookup[status]
+    def color = colorLookup.containsKey(status) ? colorLookup[status] : '';
+    wrap([$class: 'BuildUser']) {
+       user = "${BUILD_USER}"
     }
-    
-    def user = "${BUILD_USER}"
+
     def job = "${env.JOB_NAME} - #${env.BUILD_NUMBER} "
-    def normal_msg = "${status} after ${currentBuild.durationString} (<${env.BUILD_URL}|#Open>)"
-    def start_msg = "${user} trigger (<${env.BUILD_URL}|#Open>)"
-    def message = job + (status == 'STARTED' ? start_msg : normal_msg)
+    def normal_msg = "${status} after ${currentBuild.durationString}. (<${env.BUILD_URL}|#Open>)"
+    def start_msg = "${user} triggered this job. (<${env.BUILD_URL}|#Open>)"
+    def message = job + status == 'STARTED' ? start_msg : normal_msg
    
     slackSend (channel: 'test', color: color, message: message)
 }
